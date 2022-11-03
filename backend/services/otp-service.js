0213230630ca59b9903295410190f0d386babab1
anjali-tejasvi <anjalitejasvi501@gmail.com>
2022-11-03 08:42:29 +0530

    adding otp feature

--- backend/services/otp-service.js
+++ backend/services/otp-service.js
@@ -1,4 +1,11 @@
-const crypto = require('crypto')
+const crypto = require('crypto');
+const hashService = require('./hash-service');
+const smsSid = process.env.SMS_SID;
+const smsAuthToken =  process.env.SMS_AUTH_TOKEN;
+const twilio = require('twilio')(smsSid,smsAuthToken,{
+    lazyLoading: true
+})
+
 
 class OtpService{
   async  generateOtp(){
@@ -8,10 +15,23 @@ class OtpService{
     }
 
 
-    sendBySms(){}
+    async sendBySms(phone,otp){
+      return await twilio.messages.create({
+        to: phone,
+        from: process.env.SMS_FROM_NUMBER,
+        body:`Your otp is ${otp}`
+      })
+    }
 
 
-    verifyOtp(){}
+    verifyOtp(hashedOtp, data){
+          let computedHash = hashService.hashOtp(data);
+
+          if(computedHash === hashedOtp){
+            return true;
+          }
+          return false;
+    }
 }
 
 module.exports = new OtpService();
